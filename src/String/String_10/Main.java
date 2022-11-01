4887ec930aa8eac70b5dd232ab4fb6a9205badfc
idiotengineer <gurtjd97@gmail.com>
2022-11-01 20:41:21 +0900

    01-10 가장 짧은 문자 거리

--- src/String/String_10/Main.java
+++ src/String/String_10/Main.java
@@ -6,35 +6,34 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static void solution(String s, char c) {
-        int left_c_index = 0;
-        int right_c_index = s.indexOf(c);
-        int arr[] = new int[s.length()];
+    public static void solution(String s,char c) {
+        int left_index = s.indexOf(c,0);
+        int right_index = s.indexOf(c, left_index);
 
+        int[] arr = new int[s.length()];
         for (int i = 0; i < s.length(); i++) {
-            if (i == 0) {
-                arr[i] = right_c_index-i;
-                left_c_index = right_c_index;
-                right_c_index = s.indexOf('c',left_c_index+1);
-            } else if (i==s.length()-1) {
-                arr[i] = right_c_index-i;
-            } else {
-                right_c_index = s.indexOf('c',left_c_index+1);
-                arr[i] = Math.min(Math.abs(left_c_index - i), Math.abs(right_c_index - i));
-                left_c_index = right_c_index;
-            }
+
+        if (s.charAt(i) == c) {
+            left_index = right_index;
+            right_index = s.indexOf(c, left_index+1);
+            arr[i] = 0;
+            sb.append(0).append(" ");
+            continue;
         }
-        System.out.println(arr);
+        arr[i] = Math.min(Math.abs(i-left_index),Math.abs(i-right_index));
+        sb.append(arr[i]).append(" ");
     }
-
-
+}
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
         String s = st.nextToken();
         char c = st.nextToken().charAt(0);
-
         solution(s,c);
-        }
+
+        System.out.println(sb);
+    }
+
+    public static StringBuilder sb = new StringBuilder();
 }
